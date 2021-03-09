e578714619dffadcbb06f0e42ffd3c0f0d7fcbb5
Shubham Aggarwal <shubham2403tayal@gmail.com>
2021-03-09 16:22:54 +0530

    Supports File
    CLI Application

--- Cypher/Application.java
+++ Cypher/Application.java
@@ -1,5 +1,10 @@
 package encryptdecrypt;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 
 public class Application{
@@ -7,37 +12,66 @@ public class Application{
     {
         hashMap.put("-mode","enc");
         hashMap.put("-key","0");
-        hashMap.put("-data"," ");
+        hashMap.put("-data","");
     }
 
-    public void execute(String[] userArgs){
+    protected void execute(String[] userArgs) throws IOException {
         for (int i = 0; i <= userArgs.length-1 ; i+=2) {
             hashMap.put(userArgs[i], userArgs[i+1]);
         }
-        checkWhatToDo();
+        encryptDecrypt();
     }
 
-    private void checkWhatToDo(){
-        if(hashMap.get("-mode").equals("enc"))encrypt();
-        else decrypt();
-    }
-
-    private void encrypt() {
+    private void encryptDecrypt() throws IOException {
         int number = Integer.parseInt(hashMap.get("-key"));
-        char[] message = hashMap.get("-data").toCharArray();
-        for(int i = 0; i < hashMap.get("-data").length(); i++){
+        char[] message;
+        if(whatToWrite()){
+            message = message(hashMap.get("-data"));
+        }
+        else message = message(readFileAsString(hashMap.get("-in")));
+        int length = message.length;
+
+        if(checkWhatToDo()){
+            changeState(message, length, number);
+        }
+        else changeState(message, length, -number);
+
+    }
+
+    private boolean checkWhatToDo(){
+        return hashMap.get("-mode").equals("enc");
+    }
+
+    private void changeState(char[] message, int length, int number) throws IOException {
+        for(int i = 0; i < length; i++){
             message[i] = (char) (message[i] + number);
         }
-        System.out.println(message);
+        writeMethod(message);
     }
 
-    private void decrypt() {
-        int number = Integer.parseInt(hashMap.get("-key"));
-        char[] message = hashMap.get("-data").toCharArray();
-        for(int i = 0; i < hashMap.get("-data").length(); i++){
-            message[i] = (char) (message[i] - number);
-        }
-        System.out.println(message);
+    private char[] message(String message){
+        return message.toCharArray();
     }
 
+    private void writeMethod(char[] message) throws IOException {
+        if(whereToWrite()){
+            File file = new File(hashMap.get("-out"));
+            FileWriter writer = new FileWriter(file, false);
+            writer.write(message);
+            writer.close();
+
+        }else System.out.println(message);
+    }
+
+    private boolean whatToWrite(){
+        return !hashMap.get("-data").equals("");
+    }
+
+    private boolean whereToWrite(){
+        return hashMap.containsKey("-out");
+    }
+
+    private static String readFileAsString(String fileName) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(fileName)));
+    }
 }
--- Cypher/Main.java
+++ Cypher/Main.java
@@ -1,9 +1,12 @@
 package encryptdecrypt;
 
+import java.io.IOException;
+
 public class Main{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Application application = new Application();
+      //  String[] arg = "-mode enc -key 5 -data Welcom-to-hyperskill!".split(" ");
         application.execute(args);
     }
 }
