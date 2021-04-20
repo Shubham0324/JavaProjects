c86bf571d569327acffb335590bdcfbb25a011c2
Shubham Aggarwal <shubham2403tayal@gmail.com>
2021-04-20 11:48:30 +0530

    Initial Progress

---
+++ codesharingplatform/src/test/java/com/codeplatform/codesharingplatform/CodesharingplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeplatform.codesharingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesharingplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
